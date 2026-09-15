782c3520309699149392210f092d3d977413c3be
Josephover <michelcruzati@gmail.com>
2026-09-15 16:00:15 -0500

    agregar dominio de Vercel a CORS

--- src/main/java/com/barberia/barberiabackend/config/CorsConfig.java
+++ src/main/java/com/barberia/barberiabackend/config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+            "https://barberia-frontend-topaz.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
